7bd08aec340a30d023218fb58f725bc29baa1dc3
shiroemons <shiroemons@gmail.com>
2020-04-21 01:45:52 +0900

    Add original_song model

--- app/models/original.rb
+++ app/models/original.rb
@@ -1,2 +1,16 @@
 class Original < ApplicationRecord
+  has_many :original_songs, -> { order(Arel.sql('"original_songs"."track_number" ASC')) },
+    foreign_key: :original_code,
+    primary_key: :code,
+    inverse_of: :original,
+    dependent: :destroy
+
+  enum original_type: {
+    pc98: 'pc98',
+    windows: 'windows',
+    zuns_music_collection: 'zuns_music_collection',
+    akyus_untouched_score: 'akyus_untouched_score',
+    commercial_books: 'commercial_books',
+    other: 'other',
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_161158) do
+ActiveRecord::Schema.define(version: 2020_04_20_162729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,18 @@ ActiveRecord::Schema.define(version: 2020_04_20_161158) do
     t.string "karaoke_type", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "original_songs", primary_key: "code", id: :string, force: :cascade do |t|
+    t.string "original_code", null: false
+    t.string "title", null: false
+    t.string "composer", default: "", null: false
+    t.integer "track_number", null: false
+    t.boolean "is_duplicate", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["code"], name: "index_original_songs_on_code", unique: true
+    t.index ["original_code"], name: "index_original_songs_on_original_code"
   end
 
   create_table "originals", primary_key: "code", id: :string, force: :cascade do |t|
