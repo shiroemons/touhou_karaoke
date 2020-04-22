b2119fd727de8e9ec85af452fad65c786c1d9f03
shiroemons <shiroemons@gmail.com>
2020-04-22 19:01:40 +0900

    Change CSV.read to CSV.table

--- db/seeds/001_originals.rb
+++ db/seeds/001_originals.rb
@@ -2,16 +2,13 @@ require 'csv'
 
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE originals;")
 insert_data = []
-originals = CSV.read('db/fixtures/originals.tsv', col_sep: "\t")
-header = originals.shift
 now = Time.now
-originals.each do |original|
-  o = header.zip(original).to_h
+originals = CSV.table('db/fixtures/originals.tsv', { col_sep: "\t", converters: nil }).each do |o|
   insert_data << {
-    code: o['code'],
-    title: o['title'],
-    short_title: o['short_title'],
-    original_type: o['original_type'],
+    code: o[:code],
+    title: o[:title],
+    short_title: o[:short_title],
+    original_type: o[:original_type],
     created_at: now,
     updated_at: now
   }
--- db/seeds/002_original_songs.rb
+++ db/seeds/002_original_songs.rb
@@ -2,18 +2,15 @@ require 'csv'
 
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE original_songs;")
 insert_data = []
-original_songs = CSV.read('db/fixtures/original_songs.tsv', col_sep: "\t")
-header = original_songs.shift
 now = Time.now
-original_songs.each do |original_song|
-  os = header.zip(original_song).to_h
+original_songs = CSV.table('db/fixtures/original_songs.tsv', { col_sep: "\t", converters: nil }).each do |os|
   insert_data << { 
-    code: os['code'],
-    original_code: os['original_code'],
-    title: os['title'],
-    composer: os['composer'].to_s,
-    track_number: os['track_number'],
-    is_duplicate: os['is_duplicate'].to_s == '1',
+    code: os[:code],
+    original_code: os[:original_code],
+    title: os[:title],
+    composer: os[:composer].to_s,
+    track_number: os[:track_number].to_i,
+    is_duplicate: os[:is_duplicate].to_s == '1',
     created_at: now,
     updated_at: now
   }
